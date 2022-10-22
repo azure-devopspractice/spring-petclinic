56f13d114caa7548b0a7f91a1d726331931509a8
Naveen@gmial.com <naveen@gmail.com>
2022-10-22 18:03:23 +0530

    dfd

--- src/vars/sample.groovy
+++ src/vars/sample.groovy
@@ -2,15 +2,9 @@ def call () {
     pipeline {
     agent any
     stages{
-        stage('git') {
+         stage('build')  {
             steps {
-                git branch: 'develop', 
-                url: 'git@github.com:azure-devopspractice/spring-petclinic.git'
-            }
-        }
-        stage('build')  {
-            steps {
-                sh 'mvn package'
+                sh 'echo murali'
             }
         }
    }
